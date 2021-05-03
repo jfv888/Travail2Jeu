d2c91d439567c7075b32348cf02cc1a6d437f3e0
DESKTOP-5SB2VSO\jfv88 <jfv888@hotmail.fr>
2021-05-03 10:53:21 -0400

    Creations des classes et ajout de variables

--- Travail 2 Jeu/Travail 2 Jeu/Form1.cs
+++ Travail 2 Jeu/Travail 2 Jeu/Form1.cs
@@ -12,9 +12,17 @@ namespace Travail_2_Jeu
 {
     public partial class Form1 : Form
     {
+        private Game game;
+        private PlayerInput playerinput;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Travail 2 Jeu/Travail 2 Jeu/Spell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Travail_2_Jeu
+{
+    class Spell
+    {
+        private int spellNumber;
+        private Image spell;
+        private Bitmap spellBitmap;
+        private int spellWidth;
+        private int spellHeight;
+        private int spellPositionX;
+        private int spellPositionY;
+        private int spellSpeed;
+        private RectangleF spellHitbox;
+        private bool onCooldown;
+    }
+}
